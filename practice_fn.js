ea6f3266d6f39e3aeb8c6764fd4cbc562d488259
Md Noor Faraz <noorfarazn3f@gmail.com>
2024-12-10 21:27:26 +0530

    create a fn for calculate two numbers

---
+++ practice_fn.js
@@ -0,0 +1,14 @@
+// create a fn, which will return sum of num1 & num2
+function sumOfTwo(num1, num2) {
+  return num1 + num2;
+}
+let sum = sumOfTwo(3, 50);
+let value2 = sumOfTwo(4, 400);
+let value3 = sumOfTwo(86, 50);
+let value4 = sumOfTwo(100, 50);
+
+console.log("the sum of numbers are: ", sum);
+console.log("the sum of numbers are: ", value2);
+console.log("the sum of numbers are: ", value3);
+console.log("the sum of numbers are: ", value4);
+console.log("the sum of numbers are: ", value1);
